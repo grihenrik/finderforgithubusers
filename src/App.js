1f47c99cf0b6198eca095e209643a180325b5bf8
Henrik Gripenberg <henrik.gripenberg@tammidigital.fi>
2020-07-21 15:18:19 +0300

    Changed to functional approach

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, Component } from 'react';
+import React, { Fragment, useState } from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import bent from 'bent';
 import './App.css';
@@ -9,116 +9,116 @@ import { Navbar } from './layout/Navbar';
 import { About } from './pages/About';
 import { User } from './users/User';
 
-class App extends Component {
-  state ={
-    users: [],
-    user: {},
-    repos: [],
-    loading: false,
-    value: "",
-    alert: null,
-    
-    
-  }
-  
+const App = ()=> {
+  const [users, setUsers]= useState([]);
+  const [user, setUser]=useState({});
+  const [repos, setRepos]= useState([]);
+  const [loading, setLoading]= useState(false);
+  const [alert, setAlert]= useState(null);
 
-  searchGithubUsers=(search)=>{
+  const searchGithubUsers=(search)=>{
     console.log("Searching for github user containing the phrase "+search);
-    this.setState({loading: true});
+    setLoading(true);
     const getJSON = bent('json');
     let githubUsers = getJSON(`https://api.github.com/search/users?q=${search}
       &client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
       &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     githubUsers.then({headers:{"Accept":"application/vnd.github.v3+json"}});
     githubUsers.then((data)=>{
-      this.setState({users: data.items, loading: false, alert:null});
+      setUsers(data.items);
+      setLoading(false);
+      setAlert(null);
     });
   };
 
-  getGithubUser =(userToSearch)=>{
+  const getGithubUser =(userToSearch)=>{
     console.log(userToSearch);
-    this.setState({loading: true});
+    setLoading(true);
     const getJSON = bent('json');
     let githubUsers = getJSON(`https://api.github.com/users/${userToSearch}?
       client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
       &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     githubUsers.then({headers:{"Accept":"application/vnd.github.v3+json"}});
     githubUsers.then((data)=>{
-      this.setState({user: data, loading: false, alert:null});
+      setUser(data);
+      setLoading(false);
+      setAlert(null);
     });
   };
 
-  getGithubUserRepos =(gituhubUser)=>{
+  const getGithubUserRepos =(gituhubUser)=>{
     console.log(gituhubUser);
-    this.setState({loading: true});
+    setLoading( true );
     const getJSON = bent('json');
     let githubUsers = getJSON(`https://api.github.com/users/${gituhubUser}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     githubUsers.then({headers:{"Accept":"application/vnd.github.v3+json"}});
     githubUsers.then((data)=>{
-      this.setState({repos: data, loading: false, alert:null});
+      setRepos(data);
+      setLoading(false);
+      setAlert(null);
     });
   };
 
-  clearGithubUsers = ()=>this.setState({users: [], loading: false});
-
-  setAlert = (msg,type)=>{
-    this.setState({alert: {msg, type}});
-    setTimeout(()=>this.setState({alert: null}),5000);
-  };
-  static cv=""
-  captureValue = (e)=>this.setState({ [e.target.name]: e.target.value });
-  
-  
-  render(){
-    const {users, user, repos, loading} = this.state;
-    if(users){
-    let showClearButton = users.length>0?true:false;
-    return (
-      <Router>
-      <div className="App">
-          <Navbar />
-        <header className="container">
-        <Alert alert = {this.state.alert}/>
-        <Switch>
-          <Route
-            exact path="/"
-            render={props=>(
-              <Fragment>
-              <div>
-                
-                <Search 
-                  searchGithubUsers={ this.searchGithubUsers }
-                  setAlert={this.setAlert}
-                  showClear={showClearButton}
-                  clearGithubUsers={this.clearGithubUsers}
-                />
-                {console.log(showClearButton)}
-                <Users loading={loading} users={users}/>
-                </div>
-            </Fragment>
-          )}/>
-          <Route exact path='/about' component={About}/>
-          <Route exact path='/user/:login' render={props=>(
-            <User 
-              {...props } 
-              getGithubUser={this.getGithubUser}
-              getGithubUserRepos={this.getGithubUserRepos} 
-              user={user}
-              repos={repos} 
-              loading={loading}/>
-            )}/>
-
-        </Switch>
-        
-        </header>
-      </div>
-      </Router>
-    );
-    } else {
-      return "Searching..."
-    }
+  const clearGithubUsers = ()=>{
+    setUsers([]);
+    setLoading(false);
   }
+
+  const showAlert = (msg,type)=>{
+    setAlert({msg, type});
+    setTimeout(()=>setAlert(null),5000);
+  };
+
   
+  
+  
+
+  if(users){
+  let showClearButton = users.length>0?true:false;
+  return (
+    <Router>
+    <div className="App">
+        <Navbar />
+      <header className="container">
+      <Alert alert = {alert}/>
+      <Switch>
+        <Route
+          exact path="/"
+          render={props=>(
+            <Fragment>
+            <div>
+              
+              <Search 
+                searchGithubUsers={ searchGithubUsers }
+                setAlert={showAlert}
+                showClear={showClearButton}
+                clearGithubUsers={clearGithubUsers}
+              />
+              {console.log(showClearButton)}
+              <Users loading={loading} users={users}/>
+              </div>
+          </Fragment>
+        )}/>
+        <Route exact path='/about' component={About}/>
+        <Route exact path='/user/:login' render={props=>(
+          <User 
+            {...props } 
+            getGithubUser={getGithubUser}
+            getGithubUserRepos={getGithubUserRepos} 
+            user={user}
+            repos={repos} 
+            loading={loading}/>
+          )}/>
+
+      </Switch>
+      
+      </header>
+    </div>
+    </Router>
+  );
+  } else {
+    return "Searching..."
+  }
 }
 
 export default App;
--- src/users/Search.js
+++ src/users/Search.js
@@ -1,50 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-export class Search extends Component {
-    state = {
-        search: ''
-    };
 
-    static propTypes = {
-        searchGithubUsers: PropTypes.func.isRequired,
-        clearGithubUsers: PropTypes.func.isRequired,
-        showClear: PropTypes.bool.isRequired
-    
-    };
-    handleSearch = (e)=>this.setState({ [e.target.name] : e.target.value});
+const Search = ({clearGithubUsers, searchGithubUsers, setAlert, showClear})=> {
+    const [ search, setSearch ] = useState('');
 
-    submitSearch = (e)=>{
+    const handleSearch = (e)=>setSearch(e.target.value);
+
+    const submitSearch = (e)=>{
         e.preventDefault();
-        if(this.state.search===''){
-            this.props.setAlert('Please enter search parameter', 'light');
+        if(search===''){
+            setAlert('Please enter search parameter', 'light');
 
         } else{
 
-            this.props.searchGithubUsers(this.state.search);
-            this.setState({search : ""});
+            searchGithubUsers(search);
+            setSearch('');
         }
         
     }
-    
-    render() {
-        const {clearGithubUsers}=this.props;
-        return (
-            <div>
-                <form onSubmit={this.submitSearch} className="form">
-                    <input 
-                        type="text" 
-                        name="search" 
-                        placeholder="Search users..."
-                        value = {this.state.search}
-                        onChange={this.handleSearch}
-                        />
-                    <input type="submit" value="Search" className="btn btn-dartk btn-block"/>
-                </form>
-                {this.props.showClear && <button className="btn btn-light btn-block" 
-                  onClick={clearGithubUsers}>Clear</button>}
-            </div>
-        )
-    }
+
+    return (
+        <div>
+            <form onSubmit={submitSearch} className="form">
+                <input 
+                    type="text" 
+                    name="search" 
+                    placeholder="Search users..."
+                    value = {search}
+                    onChange={handleSearch}
+                    />
+                <input type="submit" value="Search" className="btn btn-dartk btn-block"/>
+            </form>
+            {showClear && <button className="btn btn-light btn-block" 
+                onClick={clearGithubUsers}>Clear</button>}
+        </div>
+    )
 }
 
+Search.propTypes = {
+    searchGithubUsers: PropTypes.func.isRequired,
+    clearGithubUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired
+
+};
 export default Search;
